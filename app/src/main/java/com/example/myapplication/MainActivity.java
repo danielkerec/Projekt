ad24afb49bdebede553d12d2156066a38e80af0d
danielkerec <46537719+danielkerec@users.noreply.github.com>
2019-05-29 22:17:10 +0200

    nov acivity prikaz restavracij, dodan toolbar v prikazu restavracij

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button prikaziRestavracije;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prikaziRestavracije = (Button) findViewById(R.id.prikazi_restavracije);
+
+        prikaziRestavracije.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Prikaz_restavracij.class));
+            }
+        });
     }
 }
